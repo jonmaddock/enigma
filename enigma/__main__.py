5465d33a8d235c94d94d87fe5b39aa355211a59b
jonmaddock <78556175+jonmaddock@users.noreply.github.com>
2021-10-17 20:23:11 +0100

    Add Morse decoder

--- enigma/__main__.py
+++ enigma/__main__.py
@@ -2,9 +2,12 @@
 from enigma.morse import Morse
 
 def main():
-    """Encode some text as Morse."""
+    """Encode some text to Morse and back again."""
     morse = Morse()
     morse.encode("test")
+
+    # Decode the Morse code back again
+    morse.decode(morse.morse)
 
 if __name__ == "__main__":
     main()
--- enigma/morse.py
+++ enigma/morse.py
@@ -1,4 +1,11 @@
-"""A Morse code encoder and decoder."""
+"""A Morse code encoder and decoder.
+
+Morse code consists of "dits", "dahs" and spaces. A dit or dah is a signal, 
+whereas a space is an absensce of signal. A dit is one unit of Morse time (or
+beat) a dah is three. Each dit or dah is followed by a space of one dit. Each 
+character is followed by a space of three dits, and words are separated by a 
+space of seven dits.
+"""
 MORSE_CODE = {
     "A": ".-",
     "B": "-...",
@@ -35,11 +42,17 @@ MORSE_CODE = {
     "7": "--...",
     "8": "---..",
     "9": "----.",
-    "0": "-----"
+    "0": "-----",
 }
 
-class Morse():
+# Define space (in "dits") at end of characters and words
+MORSE_CHAR_SPACE = " " * 3
+MORSE_WORD_SPACE = " " * 7
+
+
+class Morse:
     """Morse code encoder/decoder."""
+
     def __init__(self):
         """Initialise empty text and Morse attributes."""
         self.text = ""
@@ -52,9 +65,55 @@ class Morse():
         :type text: str
         """
         self.text = text.upper()
+        self.morse = ""
         print(f"Text: {self.text}")
 
+        # Convert each character to Morse, then add end-of-character space
         for char in self.text:
-            self.morse += MORSE_CODE[char]
+            self.morse += MORSE_CODE[char] + MORSE_CHAR_SPACE
 
         print(f"Morse: {self.morse}")
+
+    def decode(self, morse):
+        """Decode input Morse to text.
+
+        :param morse: input Morse code
+        :type morse: str
+        """
+        self.morse = morse
+        self.text = ""
+        print(f"Morse: {self.morse}")
+
+        # Break up Morse words
+        morse_words = self.morse.split(MORSE_WORD_SPACE)
+        self.decode_words(morse_words)
+
+        print(f"Text: {self.text}")
+
+    def decode_words(self, morse_words):
+        """Decode a list of Morse words.
+
+        :param morse_words: list of Morse words
+        :type morse_words: list
+        """
+        # Split each word into letters and decode them
+        for morse_word in morse_words:
+            morse_letters = morse_word.split(MORSE_CHAR_SPACE)
+            self.decode_letters(morse_letters)
+
+            # Add space after word
+            self.text += " "
+
+    def decode_letters(self, morse_letters):
+        """Decode a list of Morse letters.
+
+        :param morse_letters: list of Morse letters
+        :type morse_letters: list
+        """
+        for morse_letter in morse_letters:
+            # Look up each Morse letter to find text letter
+            for key, value in MORSE_CODE.items():
+                if value == morse_letter:
+                    # Found matching Morse; add corresponding text letter
+                    self.text += key
+                    break
