95a1acbcc7a5860e538ce2657e69fc93be4d6954
jonmaddock <78556175+jonmaddock@users.noreply.github.com>
2021-11-09 22:27:30 +0000

    Add Keyer() to play Morse audio

---
+++ enigma/keyer.py
@@ -0,0 +1,96 @@
+"""A Morse code keyer.
+
+A Morse "key" is a device with a button used to encode the carrier wave with a
+Morse signal. Morse operators use these to produce the familiar "dits" and
+"dahs" of Morse code. The Keyer class converts dots and dashes into an encoded
+carrier waveform and plays it audibly.
+"""
+import numpy as np
+import simpleaudio as sa
+
+# Dit and dah timings
+MORSE_DIT_FREQ = 10  # dits per second
+MORSE_DIT = 1
+MORSE_DAH = 3
+
+# Audio settings
+FREQUENCY = 440  # 440 Hz
+SAMPLE_RATE = 44100
+
+
+class Keyer:
+    """Convert Morse code to audio and play it."""
+
+    def __init__(self, morse):
+        """Convert Morse to playable audio.
+
+        :param morse: dot-and-dash Morse code
+        :type morse: str
+        """
+        self.signal = self.create_binary_signal(morse)
+        self.audio = self.convert_audio()
+
+    def create_binary_signal(self, morse):
+        """Converts Morse code into a binary signal.
+
+        For example, ".-   ." becomes "1011100001"
+        :param morse: dot-and-dash Morse code
+        :type morse: str
+        :return: binary Morse code signal
+        :rtype: np.ndarray
+        """
+        signal_list = []
+
+        # Convert to binary dit, dah or space
+        # Always add a space of one dit
+        for char in morse:
+            if char == ".":
+                signal_list += MORSE_DIT * [1]
+            elif char == "-":
+                signal_list += MORSE_DAH * [1]
+
+            signal_list += MORSE_DIT * [0]
+
+        # TODO Correct number of spaces: consider end of char/word following
+        # dit/dah: has one too many spaces currently
+
+        # signal_list is now list of binary digits, each representing a dit
+        # duration of on or off
+        signal = np.array(signal_list)
+        return signal
+
+    def convert_audio(self):
+        """Convert binary signal to audio.
+
+        Encode sine wave with binary signal and create playable audio.
+        :return: 16-bit audio waveform
+        :rtype: np.ndarray
+        """
+        # Stretch signal array to match the required sample rate and duration
+        samples_per_dit = int(round(SAMPLE_RATE / MORSE_DIT_FREQ))
+        signal_stretched = np.repeat(self.signal, samples_per_dit)
+
+        # Create increasing time value array of equivalent length
+        duration = signal_stretched.size / SAMPLE_RATE
+        t = np.linspace(0.0, duration, num=signal_stretched.size, endpoint=False)
+
+        # Create a sine wave at 440 Hz
+        sine = np.sin(2 * np.pi * FREQUENCY * t)
+
+        # Encode sine wave with signal
+        enc_sine = sine * signal_stretched
+
+        # Ensure that sine is in 16-bit range, normalised to maximum amplitude
+        audio = (2 ** 15 - 1) * enc_sine / (np.max(np.abs(enc_sine)))
+
+        # Convert to 16-bit data
+        audio = audio.astype(np.int16)
+        return audio
+
+    def play(self):
+        """Play Morse code."""
+        # Start playback
+        play_obj = sa.play_buffer(self.audio, 1, 2, SAMPLE_RATE)
+
+        # Wait for playback to finish before exiting
+        play_obj.wait_done()
--- enigma/morse.py
+++ enigma/morse.py
@@ -6,8 +6,7 @@ beat) a dah is three. Each dit or dah is followed by a space of one dit. Each
 character is followed by a space of three dits, and words are separated by a 
 space of seven dits.
 """
-import numpy as np
-import simpleaudio as sa
+from enigma.keyer import Keyer
 
 MORSE_CODE = {
     "A": ".-",
@@ -87,7 +86,9 @@ class Morse:
         self.text = ""
         print(f"Morse: {self.morse}")
 
-        self.play()
+        # Play Morse code sound
+        keyer = Keyer(self.morse)
+        keyer.play()
 
         # Break up Morse words
         morse_words = self.morse.split(MORSE_WORD_SPACE)
@@ -122,27 +123,3 @@ class Morse:
                     # Found matching Morse; add corresponding text letter
                     self.text += key
                     break
-
-    def play(self):
-        """Play a continuous Morse tone."""
-        frequency = 440  # 440 Hz
-        sample_rate = 44100
-        duration = 3
-
-        # Create array for points in time
-        t = np.linspace(0, duration, num=sample_rate * duration, endpoint=False)
-
-        # Create a sine wave at 440 Hz
-        note = np.sin(2 * np.pi * frequency * t)
-
-        # Ensure that note is in 16-bit range, normalised to maximum amplitude
-        audio = (2 ** 15 - 1) * note / (np.max(np.abs(note)))
-
-        # Convert to 16-bit data
-        audio = audio.astype(np.int16)
-
-        # Start playback
-        play_obj = sa.play_buffer(audio, 1, 2, sample_rate)
-
-        # Wait for playback to finish before exiting
-        play_obj.wait_done()
